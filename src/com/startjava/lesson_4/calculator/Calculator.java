da84e618c22ee29dfc61dccfaf4e930aee307d4b
Andrei <livanovandrej99@gmail.com>
2020-06-06 14:13:31 +0700

    implement_HW4

--- src/com/startjava/lesson_4/calculator/Calculator.java
+++ src/com/startjava/lesson_4/calculator/Calculator.java
@@ -17,22 +17,22 @@ public class Calculator {
 		parseExpression(expression);
 		switch (sign) {
 			case '+':
-				System.out.println("Результат равен = " + Math.addExact(num1, num2));
+				System.out.println("Результат равен: " + Math.addExact(num1, num2));
 				break;
 			case '-':
-				System.out.println("Результат равен = " + Math.subtractExact(num1, num2));
+				System.out.println("Результат равен: " + Math.subtractExact(num1, num2));
 				break;
 			case '*':
-				System.out.println("Результат равен = " + Math.multiplyExact(num1, num2));
+				System.out.println("Результат равен: " + Math.multiplyExact(num1, num2));
 				break;
 			case '/':
-				System.out.println("Результат равен = " + Math.floorDiv(num1, num2));
+				System.out.println("Результат равен: " + Math.floorDiv(num1, num2));
 				break;
 			case '^':
-				System.out.println("Результат равен = " + (int) Math.pow(num1, num2));
+				System.out.println("Результат равен: " + (int) Math.pow(num1, num2));
 				break;
 			case '%':
-				System.out.println("Результат равен = " + (num1 % num2));
+				System.out.println("Результат равен: " + (num1 % num2));
 				break;
 			default:
 				System.out.println("Error!");
